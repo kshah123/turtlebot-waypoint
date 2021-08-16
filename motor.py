933db4280951a9043ef43b9743f6e216a6149bb2
kshah123 <kshahman11@gmail.com>
2021-08-15 23:38:38 -0400

    Added motor class

---
+++ motor.py
@@ -0,0 +1,17 @@
+class motor:
+    def __init__(self, Kt, Ke, Resistance, Inductance, Damping_coeff):
+        self.kt = Kt
+        self.ke = Ke
+        self.R = Resistance
+        self.L = Inductance
+        self.b = Damping_coeff
+        self.back_emf_prev = 0
+        self.didt_prev = 0
+        self.i_int = 0
+    def getOutputs(self, V, dt, ang_vel):
+        didt = (V - self.R * self.i_int - self.ke*self.back_emf_prev)/self.L
+        self.i_int += dt/2*(didt+self.didt_prev)
+        self.didt_prev = didt
+        self.back_emf_prev = self.R * self.i_int
+        T_net = self.kt*self.i_int-self.b*ang_vel
+        return T_net
