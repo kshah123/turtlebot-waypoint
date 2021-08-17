06eb503d4dc80705cb65f4e0a392551133103af5
kshah123 <kshahman11@gmail.com>
2021-08-17 12:07:51 -0400

    changed wheel class layout

--- wheel.py
+++ wheel.py
@@ -1,7 +1,9 @@
 class wheel(): ## ASSUME NO SLIP FOR THIS MODEL
-    def __init__(self,T_motor,r_wheel):
-        self.F_wheel = T_motor/r_wheel
-    def getForce(self):
-        return self.F_wheel
-    def setForce(self,T_motor,r_wheel):
-        self.F_wheel = T_motor/r_wheel
+    def __init__(self,moment_of_inertia,mass,wheel_motor, radius):
+        self.I = moment_of_inertia
+        self.m = mass
+        self.motor = wheel_motor
+        self.R = radius
+    def getAppliedForce(self):
+        return self.motor.getOutputs() / self.R
+        
